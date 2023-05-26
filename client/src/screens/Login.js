4db73e8aaf9e3a0d2a76c86d894674d5e2e36fbc
Mohsinalisyed <113241363+Mohsinalisyed@users.noreply.github.com>
2023-05-26 15:59:03 +0500

    ok

--- client/src/screens/Login.js
+++ client/src/screens/Login.js
@@ -41,7 +41,7 @@ export default function Login() {
         <Navbar />
       </div>
       <div className='container'>
-        <form className='w-50 m-auto mt-5 border bg-dark border-success rounded' onSubmit={handleSubmit}>
+        <form className='.form-container mt-5 border bg-dark border-success rounded' onSubmit={handleSubmit}>
           <div className="m-3">
             <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
             <input type="email" className="form-control" name='email' value={credentials.email} onChange={onChange} aria-describedby="emailHelp" />
--- client/src/screens/Signup.js
+++ client/src/screens/Signup.js
@@ -71,7 +71,7 @@ export default function Signup() {
       </div>
 
       <div className='container' >
-        <form className='w-50 m-auto mt-5 border bg-dark border-success rounded' onSubmit={handleSubmit}>
+        <form className='.form-container  mt-5 border bg-dark border-success rounded' onSubmit={handleSubmit}>
           <div className="m-3">
             <label htmlFor="name" className="form-label">Name</label>
             <input type="text" className="form-control" name='name' value={credentials.name} onChange={onChange} aria-describedby="emailHelp" />
