4c1553d2f90c44f203428c4b4f9d3febf66af60f
Mohsin512 <mohsin.syed@ideofuzion.com>
2023-11-17 13:24:49 -0700

    add hook form

--- client/src/screens/Signup.js
+++ client/src/screens/Signup.js
@@ -1,52 +1,46 @@
-import React, { useState } from 'react'
-import { useNavigate, Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import { config } from '../Config';
 import { useMutation } from 'react-query';
 import { signUpUser } from '../services/Auth';
 import axios from 'axios';
- const  Signup =()=> {
-  const [credentials, setCredentials] = useState({ name: "", email: "", password: "", geolocation: "" })
-  const [address, setAddress] = useState("");
-  const navigate = useNavigate()
+import { useForm} from 'react-hook-form';
+
+const Signup = () => {
+  const [address, setAddress] = useState('');
+  const navigate = useNavigate();
   const mutation = useMutation(signUpUser);
-  const handleClick = async (e) => {
-    e.preventDefault();
-    const navLocation = () => {
-      return new Promise((res, rej) => {
+  const {handleSubmit, setValue, register, formState: { errors } } = useForm();
+
+  const handleClick = async () => {
+    try {
+      const position = await new Promise((res, rej) => {
         navigator.geolocation.getCurrentPosition(res, rej);
       });
+      const { latitude, longitude } = position.coords;
+
+      const { data: { location } } = await axios.post(`${config.Port}/api/auth/getlocation`, {
+        latlong: { lat: latitude, long: longitude },
+      });
+
+      setAddress(location);
+      setValue('address', location);
+    } catch (error) {
+      console.error('An error occurred during fetching location:', error);
     }
-    const latlong = await navLocation().then(res => {
-      const latitude = res.coords.latitude;
-      const longitude = res.coords.longitude;
-      return [latitude, longitude]
-    })
-    // console.log(latlong)
-    const [lat, long] = latlong
-    const { data: { location } } = await axios.post(`${config.Port}/api/auth/getlocation`, {
-      latlong: { lat, long },
-    });
-    setAddress(location);
-    setCredentials({ ...credentials, [e.target.name]: location })
-  }
+  };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
+  const onSubmit = async (data) => {
     try {
-      const { data } = await mutation.mutateAsync(credentials);
-        localStorage.setItem('token', data?.authToken);
-        navigate('/login');
+      const { data: response } = await mutation.mutateAsync(data);
+      localStorage.setItem('token', response?.authToken);
+      navigate('/login');
     } catch (error) {
       console.error('An error occurred during user submission:', error);
       // Handle error as needed
     }
   };
-
-  const onChange = (e) => {
-    setCredentials({ ...credentials, [e.target.name]: e.target.value })
-  }
 
   return (
     <div style={{ backgroundImage: 'url("https://images.pexels.com/photos/1565982/pexels-photo-1565982.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")', backgroundSize: 'cover', height: '100vh' }}>
@@ -54,34 +48,39 @@ import axios from 'axios';
         <Navbar />
       </div>
 
-      <div className='container' >
-        <form className='form-container  mt-5 border bg-dark border-success rounded' onSubmit={handleSubmit}>
+      <div className='container'>
+        <form className='form-container mt-5 border bg-dark border-success rounded' onSubmit={handleSubmit(onSubmit)}>
           <div className="m-3">
             <label htmlFor="name" className="form-label">Name</label>
-            <input type="text" className="form-control" name='name' value={credentials.name} onChange={onChange} aria-describedby="emailHelp" />
+            <input type="text" className={`form-control ${errors.name ? 'is-invalid' : ''}`} {...register('name', { required: 'Name is required' })} />
+            {errors.name && <div className="invalid-feedback">{errors.name.message}</div>}
           </div>
           <div className="m-3">
             <label htmlFor="email" className="form-label">Email address</label>
-            <input type="email" className="form-control" name='email' value={credentials.email} onChange={onChange} aria-describedby="emailHelp" />
+            <input type="email" className={`form-control ${errors.email ? 'is-invalid' : ''}`} {...register('email', { required: 'Email is required' })} />
+            {errors.email && <div className="invalid-feedback">{errors.email.message}</div>}
           </div>
           <div className="m-3">
             <label htmlFor="address" className="form-label">Address</label>
             <fieldset>
-              <input type="text" className="form-control" name='address' placeholder='"Click below for fetching address"' value={address} onChange={(e) => setAddress(e.target.value)} aria-describedby="emailHelp" />
+              <input type="text" className={`form-control ${errors.address ? 'is-invalid' : ''}`} {...register('address', { required: 'Address is required' })} placeholder='Click below for fetching address' value={address} onChange={(e) => setAddress(e.target.value)} />
+              {errors.address && <div className="invalid-feedback">{errors.address.message}</div>}
             </fieldset>
           </div>
           <div className="m-3">
-            <button type="button" onClick={handleClick} name="geolocation" className=" btn btn-success">Click for current Location </button>
+            <button type="button" onClick={handleClick} name="geolocation" className="btn btn-success">Click for current Location</button>
           </div>
           <div className="m-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-            <input type="password" className="form-control" value={credentials.password} onChange={onChange} name='password' />
+            <label htmlFor="password" className="form-label">Password</label>
+            <input type="password" className={`form-control ${errors.password ? 'is-invalid' : ''}`} {...register('password', { required: 'Password is required' })} />
+            {errors.password && <div className="invalid-feedback">{errors.password.message}</div>}
           </div>
           <button type="submit" className="m-3 btn btn-success">Submit</button>
           <Link to="/login" className="m-3 mx-1 btn btn-danger">Already a user</Link>
         </form>
       </div>
     </div>
-  )
-}
-export default Signup
+  );
+};
+
+export default Signup;
